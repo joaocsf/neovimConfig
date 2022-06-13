b6ecd7adea7f716ebf381b3e3cd3e5fa484cef49
joaocsf <joaocsf.se@gmail.com>
2022-06-13 19:37:24 +0100

    Update telescope layout strategy

--- lua/configs/telescope.lua
+++ lua/configs/telescope.lua
@@ -1,6 +1,39 @@
 local ok, telescope = pcall(require, 'telescope')
 if ok then
   telescope.setup {
+    defaults = {
+      layout_strategy = 'vertical',
+      layout_config = {
+        bottom_pane = {
+          height = 25,
+          preview_cutoff = 120,
+          prompt_position = 'top'
+        },
+        center = {
+          height = 0.4,
+          preview_cutoff = 40,
+          prompt_position = 'top',
+          width = 0.5
+        },
+        cursor = {
+          height = 0.9,
+          preview_cutoff = 40,
+          width = 0.8
+        },
+        horizontal = {
+          height = 0.9,
+          preview_cutoff = 120,
+          prompt_position = 'bottom',
+          width = 0.8
+        },
+        vertical = {
+          height = 0.9,
+          preview_cutoff = 40,
+          prompt_position = 'bottom',
+          width = 0.8
+        }
+      }
+    },
     extensions = {
       fzf = {
         fuzzy = true, -- false will only do exact matching
