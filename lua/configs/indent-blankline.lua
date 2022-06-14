b5f4e8d132f0a9c6c0343ce5e9657bf546cc5016
joaocsf <joaocsf.se@gmail.com>
2022-06-14 22:14:00 +0100

    Update end of line character

--- lua/configs/indent-blankline.lua
+++ lua/configs/indent-blankline.lua
@@ -3,7 +3,7 @@ if ok then
   vim.opt.list = true
 
   vim.opt.listchars:append 'space:⋅'
-  vim.opt.listchars:append 'eol:↴'
+  vim.opt.listchars:append 'eol:'
 
   blankline.setup {
     show_end_of_line = true,
