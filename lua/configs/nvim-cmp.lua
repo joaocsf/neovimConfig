17b9cdee75032931ed1ef420136a2639a4d98f66
joaocsf <joaocsf.se@gmail.com>
2022-06-17 19:09:06 +0100

    Fix fall unpack

--- lua/configs/nvim-cmp.lua
+++ lua/configs/nvim-cmp.lua
@@ -31,7 +31,7 @@ if ok then
   local ok_luasnip, luasnip = pcall(require, 'luasnip')
 
   local function has_words_before()
-    local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
     return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match '%s' == nil
   end
 
