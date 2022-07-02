454fb8c509e49b397a4205eedf9d2cfb56cabf36
joaocsf <joaocsf.se@gmail.com>
2022-07-02 14:08:05 +0100

    Add vimwiki to the `prose-lint` file types

--- lua/configs/null-ls.lua
+++ lua/configs/null-ls.lua
@@ -1,11 +1,16 @@
 local ok, nls = pcall(require, 'null-ls')
 
 if ok then
+
+  local proselintSettings = {
+    filetypes = { 'markdown', 'tex', 'vimwiki' }
+  }
+
   nls.setup {
     fallback_severity = vim.diagnostic.severity.INFO,
     sources = {
-      nls.builtins.code_actions.proselint,
-      nls.builtins.diagnostics.proselint,
+      nls.builtins.code_actions.proselint.with(proselintSettings),
+      nls.builtins.diagnostics.proselint.with(proselintSettings),
       nls.builtins.code_actions.shellcheck,
       nls.builtins.diagnostics.misspell,
       nls.builtins.completion.spell,
