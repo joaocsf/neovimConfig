2401378ffb2417c37f38f38dc60c3fbfbefbfb4e
joaocsf <joaocsf.se@gmail.com>
2022-07-02 13:33:38 +0100

    Set default `null-ls` fallback severity option

--- lua/configs/null-ls.lua
+++ lua/configs/null-ls.lua
@@ -1,8 +1,11 @@
 local ok, nls = pcall(require, 'null-ls')
+
 if ok then
   nls.setup {
+    fallback_severity = vim.diagnostic.severity.INFO,
     sources = {
       nls.builtins.code_actions.proselint,
+      nls.builtins.diagnostics.proselint,
       nls.builtins.code_actions.shellcheck,
       nls.builtins.diagnostics.misspell,
       nls.builtins.completion.spell,
