72a78c0ce1a8c31a927bc8d9f94d141a753fb3ca
joaocsf <joaocsf.se@gmail.com>
2022-07-31 18:19:03 +0100

    Set default `max_width` when displaying LSP float windows

--- lua/configs/lsp/handlers.lua
+++ lua/configs/lsp/handlers.lua
@@ -30,12 +30,13 @@ function M.setup()
     },
   }
 
-  vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, { border = 'rounded' })
+  local float_opts = {
+    border = 'rounded',
+    max_width = 90,
+  }
 
-  vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(
-    vim.lsp.handlers.signature_help,
-    { border = 'rounded' }
-  )
+  vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, float_opts)
+  vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, float_opts)
 end
 
 local function lsp_highlight_document(client)
