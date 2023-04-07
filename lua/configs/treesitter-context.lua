91f5b8cd32f06d10a95e28148f3a336d7ab3cd70
João Ferreira <joaocsf.se@gmail.com>
2023-04-07 16:39:00 +0100

    Add queries for `hocon`  and `scala`

--- lua/configs/treesitter-context.lua
+++ lua/configs/treesitter-context.lua
@@ -1,24 +1,9 @@
 local ok, ts_context = pcall(require, 'treesitter-context')
 
 if ok then
-
   ts_context.setup {
     enable = true,
     max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
     trim_scope = 'outer',
-    patterns = {
-      javascript = {
-        'pair'
-      },
-      hocon = {
-        'pair'
-      },
-      scala = {
-        'infix_expression',
-        'object_definition',
-        'call_expression'
-      }
-    }
   }
-
 end
