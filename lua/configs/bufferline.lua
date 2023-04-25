33c0fe9931bd83740ab13325920c7fcb99596817
João Ferreira <joaocsf.se@gmail.com>
2023-04-25 22:04:50 +0100

    Update bufferline defaults

--- lua/configs/bufferline.lua
+++ lua/configs/bufferline.lua
@@ -2,10 +2,11 @@ local ok, bufferline = pcall(require, 'bufferline')
 if ok then
   bufferline.setup {
     options = {
+      mode = 'buffers',
       offsets = {
         { filetype = 'NvimTree', text = '', padding = 1 },
         { filetype = 'neo-tree', text = '', padding = 1 },
-        { filetype = 'Outline', text = '', padding = 1 },
+        { filetype = 'Outline',  text = '', padding = 1 },
       },
       buffer_close_icon = '窱',
       modified_icon = '',
@@ -14,6 +15,8 @@ if ok then
       max_prefix_length = 13,
       tab_size = 20,
       separator_style = 'thin',
+      show_tab_indicators = true,
+      enforce_regular_tabs = true
     },
   }
 end
