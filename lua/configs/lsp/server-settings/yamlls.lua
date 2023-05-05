38ea27c49776f8365127f97b83f3c4cc0f59ece7
João Ferreira <jferreira@kevel.co>
2023-05-05 13:26:14 +0100

    Add yamlls default settings

---
+++ lua/configs/lsp/server-settings/yamlls.lua
@@ -0,0 +1,9 @@
+return {
+  settings = {
+    yaml = {
+      schemas = {
+        ['https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v2.0/schema.json'] = '*/*api-docs.yaml',
+      },
+    }
+  }
+}
