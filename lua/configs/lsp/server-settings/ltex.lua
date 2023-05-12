6f4bbec985e0ed2c59f625aab2a948c9b1656b9b
João Ferreira <joaocsf.se@gmail.com>
2023-05-12 20:47:28 +0100

    Fix `ltex` undefined access

--- lua/configs/lsp/server-settings/ltex.lua
+++ lua/configs/lsp/server-settings/ltex.lua
@@ -13,7 +13,7 @@ local opts = {
 if ok then
   local tbl_deep_extend = vim.tbl_deep_extend
   opts = tbl_deep_extend('force', opts, {
-    filetypes = { 'vimwiki', 'text', 'txt', unpack(lspconfig['ltex'].filetypes) },
+    filetypes = { 'vimwiki', 'text', 'txt', unpack(lspconfig['ltex'].document_config.default_config.filetypes) },
     get_language_id = function(arg, filetype)
       local language_id = language_id_mapping[filetype]
       if language_id then
