2687ad63e46b5d15e8d9e701616954afb71154ba
João Ferreira <joaocsf.se@gmail.com>
2023-05-12 21:12:27 +0100

    Disable `null-ls`

--- lua/configs/null-ls.lua
+++ lua/configs/null-ls.lua
@@ -1,16 +1,1 @@
 local ok, nls = pcall(require, 'null-ls')
-
-if ok then
-  local proselintSettings = {
-    filetypes = { 'markdown', 'tex', 'vimwiki' }
-  }
-
-  nls.setup {
-    fallback_severity = vim.diagnostic.severity.INFO,
-    sources = {
-      nls.builtins.code_actions.proselint.with(proselintSettings),
-      nls.builtins.diagnostics.proselint.with(proselintSettings),
-      nls.builtins.code_actions.shellcheck,
-    },
-  }
-end
