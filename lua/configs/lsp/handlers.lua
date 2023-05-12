9c401859cf74342bcc451d1e6f539d438cb135b4
João Ferreira <joaocsf.se@gmail.com>
2023-05-12 20:56:23 +0100

    Update diagnostic sign hint icon

--- lua/configs/lsp/handlers.lua
+++ lua/configs/lsp/handlers.lua
@@ -8,7 +8,7 @@ function M.setup()
   local signs = {
     { name = 'DiagnosticSignError', text = '' },
     { name = 'DiagnosticSignWarn',  text = '' },
-    { name = 'DiagnosticSignHint',  text = '󱇮' },
+    { name = 'DiagnosticSignHint',  text = '󰌵' },
     { name = 'DiagnosticSignInfo',  text = '' },
   }
 
