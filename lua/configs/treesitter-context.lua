89c3d866d2e06b31b029c6a6d23501401fc6e876
João Ferreira <jferreira@kevel.co>
2023-09-08 09:20:06 +0100

    Fix treesitter context to one line per context

--- lua/configs/treesitter-context.lua
+++ lua/configs/treesitter-context.lua
@@ -4,6 +4,7 @@ if ok then
   ts_context.setup {
     enable = true,
     max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+    multiline_threshold = 1,
     trim_scope = 'outer',
   }
 end
