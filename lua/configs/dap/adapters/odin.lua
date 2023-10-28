e818feef41b2f44c431b4801901131456cd797c1
João Ferreira <joaocsf.se@gmail.com>
2023-10-28 17:46:36 +0100

    Add debug adapter for `odin`

---
+++ lua/configs/dap/adapters/odin.lua
@@ -0,0 +1,5 @@
+return {
+  id = 'cppdbg',
+  type = 'executable',
+  command = os.getenv 'OPEN_DEBUG_AD7_PATH'
+}
---
+++ lua/configs/dap/configurations/odin.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    name = 'Launch file',
+    type = 'odin',
+    request = 'launch',
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+    cwd = '${workspaceFolder}',
+    stopAtEntry = true,
+  },
+}
