2a833108952aa0e081ec20256f64e2012e28449a
João Ferreira <jferreira@kevel.co>
2024-01-03 08:03:48 +0000

    Add option to attatch to scala dap session

--- lua/configs/dap/configurations/scala.lua
+++ lua/configs/dap/configurations/scala.lua
@@ -6,5 +6,13 @@ return {
     metals = {
       runType = 'runOrTestFile'
     },
+  },
+  {
+    type = 'scala',
+    name = 'Attach to localhost:5005',
+    request = 'attach',
+    hostName = 'localhost',
+    port = 5005,
+    buildTarget = 'adforecaster-simulator'
   }
 }
