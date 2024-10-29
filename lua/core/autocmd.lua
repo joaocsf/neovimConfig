a02f9d7f093c5c82a15ab197d99fe6b6523c83fd
João Ferreira <jferreira@kevel.co>
2024-10-29 09:18:58 +0000

    Use alternative `hl` method

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -4,7 +4,7 @@ local augroup = vim.api.nvim_create_augroup
 local group = augroup('Highlight Yank', { clear = true })
 autocmd('TextYankPost', {
   callback = function()
-    require 'vim.highlight'.on_yank { higroup = 'IncSearch', timeout = 200 }
+    vim.hl.on_yank { higroup = 'IncSearch', timeout = 200 }
   end,
   group = group,
 })
@@ -27,11 +27,11 @@ autocmd('BufReadPost', {
   end
 })
 
--- TODO: Temporary Fix: https://github.com/nvim-telescope/telescope.nvim/issues/2027
-autocmd('WinLeave', {
-  callback = function()
-    if vim.bo.ft == 'TelescopePrompt' and vim.fn.mode() == 'i' then
-      vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Esc>', true, false, true), 'i', false)
-    end
-  end,
-})
+-- -- TODO: Temporary Fix: https://github.com/nvim-telescope/telescope.nvim/issues/2027
+-- autocmd('WinLeave', {
+--   callback = function()
+--     if vim.bo.ft == 'TelescopePrompt' and vim.fn.mode() == 'i' then
+--       vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Esc>', true, false, true), 'i', false)
+--     end
+--   end,
+-- })
