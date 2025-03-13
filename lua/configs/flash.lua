b791ca9490752fbcbbb6dda05c761a3b6592c379
João Ferreira <jferreira@kevel.co>
2025-03-13 15:27:05 +0000

    Allow using Surround in visual selection

--- lua/configs/flash.lua
+++ lua/configs/flash.lua
@@ -4,7 +4,7 @@ if ok then
   local map = vim.keymap.set
 
   map({ 'n', 'x', 'o' }, 's',     function() flash.jump() end,              { desc = 'Flash' })
-  map({ 'n', 'x', 'o' }, 'S',     function() flash.treesitter() end,        { desc = 'Flash Treesitter' })
+  map({ 'n', 'o' },      'S',     function() flash.treesitter() end,        { desc = 'Flash Treesitter' })
   map({ 'o' },           'r',     function() flash.remote() end,            { desc = 'Remote Flash' })
   map({ 'o' },           'R',     function() flash.treesitter_search() end, { desc = 'Treesitter Search' })
   map({ 'c' },           '<c-s>', function() flash.toggle() end,            { desc = 'Toggle Flash Search' })
