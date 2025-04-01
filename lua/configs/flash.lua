ecab5c9eed08d5053cc3392a25a73fae019d4b6e
João Ferreira <jferreira@kevel.co>
2025-04-01 17:04:55 +0100

    Remap leader

--- lua/configs/flash.lua
+++ lua/configs/flash.lua
@@ -3,6 +3,8 @@ local ok, flash = pcall(require, 'flash')
 if ok then
   local map = vim.keymap.set
 
+  flash.setup()
+
   map({ 'n', 'x', 'o' }, 's',     function() flash.jump() end,              { desc = 'Flash' })
   map({ 'n', 'o' },      'S',     function() flash.treesitter() end,        { desc = 'Flash Treesitter' })
   map({ 'o' },           'r',     function() flash.remote() end,            { desc = 'Remote Flash' })
