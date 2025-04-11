99e4597624288c644726bb57a73d74e1a771a600
João Ferreira <jferreira@kevel.co>
2025-04-11 21:16:55 +0100

    Update symbols

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -41,7 +41,7 @@ vim.opt.list = true
 vim.opt.listchars:append 'leadmultispace:⋅│'
 vim.opt.listchars:append 'tab:» '
 vim.opt.listchars:append 'space:⋅'
-vim.opt.listchars:append 'eol:'
+vim.opt.listchars:append 'eol:▾'
 
 vim.opt.termguicolors = true
 
