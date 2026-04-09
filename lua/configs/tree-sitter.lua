83a263caaebd16052040b76f0dcc406883591b4c
João Ferreira <joaocsf.se@gmail.com>
2026-04-09 19:23:42 +0100

    Enable tree-sitter highlight

--- lua/configs/tree-sitter.lua
+++ lua/configs/tree-sitter.lua
@@ -1,4 +1,15 @@
 local ok, ts = pcall(require, 'nvim-treesitter')
 if ok then
   ts.setup()
+  vim.api.nvim_create_autocmd('FileType', {
+    callback = function()
+      -- syntax highlighting, provided by Neovim
+      pcall(vim.treesitter.start)
+      -- folds, provided by Neovim
+      -- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+      -- vim.wo.foldmethod = 'expr'
+      -- indentation, provided by nvim-treesitter
+      vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+    end,
+  })
 end
