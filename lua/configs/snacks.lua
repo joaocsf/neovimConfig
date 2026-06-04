b67e6486f7be2ea56d8cfa354afdba9d938803ee
João Ferreira <joaocsf.se@gmail.com>
2026-06-04 22:14:30 +0100

    Enable snacks image and bigfile

--- lua/configs/snacks.lua
+++ lua/configs/snacks.lua
@@ -3,6 +3,15 @@ local ok, snacks = pcall(require, 'snacks')
 
 if ok then
   snacks.setup {
+    bigfile = {
+      enabled = true
+    },
+    image = { enabled = true,
+      doc = {
+        enabled = true,
+        inline = true,
+      }
+    },
     dashboard = { enable = true,
       sections = {
         { height = 9,         indent = 20, padding = 3, section = "terminal", cmd = "cat '" .. vim.fn.stdpath('config') .. '/dashboard/logo.txt' .. "'" },
